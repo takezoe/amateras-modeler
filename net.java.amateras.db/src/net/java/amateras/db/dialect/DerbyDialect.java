94b69e74091b60cc81ead97b1391af4b76ace280
takezoe <takezoe@gmail.com>
2025-10-10 15:20:54 +0900

    Update DerbyDialect

--- net.java.amateras.db/src/net/java/amateras/db/dialect/DerbyDialect.java
+++ net.java.amateras.db/src/net/java/amateras/db/dialect/DerbyDialect.java
@@ -10,35 +10,27 @@ import net.java.amateras.db.visual.model.TableModel;
 public class DerbyDialect extends AbstractDialect {
 
 	private static final IColumnType[] COLUMN_TYPES = {
-		new ColumnType("BOOLEAN", Messages.getResourceString("type.boolean"), true, Types.BOOLEAN),
-		new ColumnType("BIGINT", Messages.getResourceString("type.integer"), true, Types.BIGINT),
-		new ColumnType("VARCHAR", Messages.getResourceString("type.string"),true, Types.VARCHAR),
-		new ColumnType("LONG VARCHAR", Messages.getResourceString("type.string"),true, Types.VARCHAR),
-		new ColumnType("CHAR", Messages.getResourceString("type.char"), true, Types.CHAR),
-		new ColumnType("CHARACTER", Messages.getResourceString("type.char"), true, Types.CHAR),
+		new ColumnType("SMALLINT", Messages.getResourceString("type.integer"), false, Types.SMALLINT),
+		new ColumnType("INTEGER", Messages.getResourceString("type.integer"), false, Types.INTEGER),
+		new ColumnType("INT", Messages.getResourceString("type.integer"), false, Types.INTEGER),
+		new ColumnType("BIGINT", Messages.getResourceString("type.integer"), false, Types.BIGINT),
 		new ColumnType("DECIMAL", Messages.getResourceString("type.real"), true, Types.DECIMAL),
-		new ColumnType("DEC", Messages.getResourceString("type.real"), true, Types.DECIMAL),
-		new ColumnType("NUMERIC", Messages.getResourceString("type.numeric"), true, Types.NUMERIC),
-		new ColumnType("NUM", Messages.getResourceString("type.numeric"), true, Types.NUMERIC),
-		new ColumnType("INT", Messages.getResourceString("type.integer"), true, Types.INTEGER),
-		new ColumnType("INTEGER", Messages.getResourceString("type.integer"), true, Types.INTEGER),
-		new ColumnType("DOUBLE", Messages.getResourceString("type.real"), true, Types.DOUBLE),
-		new ColumnType("FLOAT", Messages.getResourceString("type.real"), true, Types.FLOAT),
+		new ColumnType("NUMERIC", Messages.getResourceString("type.numeric"), false, Types.NUMERIC),
+		new ColumnType("REAL", Messages.getResourceString("type.real"), false, Types.REAL),
+		new ColumnType("DOUBLE PRECISION", Messages.getResourceString("type.real"), false, Types.DOUBLE),
+		new ColumnType("DOUBLE", Messages.getResourceString("type.real"), false, Types.DOUBLE),
+		new ColumnType("CHAR", Messages.getResourceString("type.char"), true, Types.CHAR),
+		new ColumnType("VARCHAR", Messages.getResourceString("type.string"),true, Types.VARCHAR),
+		new ColumnType("LONG VARCHAR", Messages.getResourceString("type.string"),false, Types.VARCHAR),
+		new ColumnType("CHAR FOR BIT DATA", Messages.getResourceString("type.binary"), true, Types.BINARY),
+		new ColumnType("VARCHAR FOR BIT DATA", Messages.getResourceString("type.binary"), false, Types.BINARY),
+		new ColumnType("CLOB", Messages.getResourceString("type.string"), false, Types.CLOB),
+		new ColumnType("BLOB", Messages.getResourceString("type.binary"), false, Types.BLOB),
 		new ColumnType("DATE", Messages.getResourceString("type.date"), false, Types.DATE),
 		new ColumnType("TIME", Messages.getResourceString("type.time"), false, Types.TIME),
 		new ColumnType("TIMESTAMP", Messages.getResourceString("type.datetime"), false, Types.TIMESTAMP),
-		new ColumnType("REAL", Messages.getResourceString("type.real"), true, Types.REAL),
-		new ColumnType("SMALLINT", Messages.getResourceString("type.integer"), true, Types.SMALLINT),
-		//new ColumnType("TINYINT", Messages.getResourceString("type.integer"), true, Types.TINYINT),
-		new ColumnType("CHARACTER VARYING FOR BIT DATA", Messages.getResourceString("type.binary"), true, Types.BINARY),
-		new ColumnType("CHAR VARYING FOR BIT DATA", Messages.getResourceString("type.binary"), true, Types.BINARY),
-		new ColumnType("VARCHAR FOR BIT DATA", Messages.getResourceString("type.binary"), true, Types.BINARY),
+		new ColumnType("BOOLEAN", Messages.getResourceString("type.boolean"), false, Types.BOOLEAN),
 		// TODO: Support following TYPE
-		//   BLOB
-		//   CLOB
-		//   CHARACTER LARGE OBJECT
-		//   CHAR LARGE OBJECT
-        //   DBCLOB
 		//   XML
 	};
 
