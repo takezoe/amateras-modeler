c8267d8b3dbd80b6b3f1e60f77f91fbaef483c3c
takezoe <takezoe@gmail.com>
2025-10-10 14:55:38 +0900

    Update HsqldbDialect

--- net.java.amateras.db/src/net/java/amateras/db/dialect/HsqldbDialect.java
+++ net.java.amateras.db/src/net/java/amateras/db/dialect/HsqldbDialect.java
@@ -12,31 +12,32 @@ public class HsqldbDialect extends AbstractDialect {
 //	private static final String DB_NAME = "hsqldb";
 
 	private static final IColumnType[] COLUMN_TYPES = {
-		new ColumnType("INT", Messages.getResourceString("type.integer"), false, Types.INTEGER),
+		new ColumnType("TINYINT", Messages.getResourceString("type.integer"), false, Types.TINYINT),
+		new ColumnType("SMALLINT", Messages.getResourceString("type.integer"), false, Types.SMALLINT),
 		new ColumnType("INTEGER", Messages.getResourceString("type.integer"), false, Types.INTEGER),
-		new ColumnType("DOUBLE", Messages.getResourceString("type.real"), false, Types.DOUBLE),
-		new ColumnType("FLOAT", Messages.getResourceString("type.real"), false, Types.FLOAT),
-		new ColumnType("VARCHAR", Messages.getResourceString("type.string"),true, Types.VARCHAR),
-		new ColumnType("VARCHAR_IGNORECASE", Messages.getResourceString("type.string"), true, Types.VARCHAR),
-		new ColumnType("CHAR", Messages.getResourceString("type.char"), true, Types.CHAR),
-		new ColumnType("CHARACTER", Messages.getResourceString("type.char"), true, Types.CHAR),
+		new ColumnType("INT", Messages.getResourceString("type.integer"), false, Types.INTEGER),
+		new ColumnType("BIGINT", Messages.getResourceString("type.integer"), false, Types.BIGINT),
 		new ColumnType("DECIMAL", Messages.getResourceString("type.real"), true, Types.DECIMAL),
 		new ColumnType("NUMERIC", Messages.getResourceString("type.numeric"), true, Types.NUMERIC),
-		new ColumnType("BOOLEAN", Messages.getResourceString("type.boolean"), true, Types.BOOLEAN),
-		new ColumnType("BIT", Messages.getResourceString("type.bit"), true, Types.BIT),
-		new ColumnType("TINYINT", Messages.getResourceString("type.integer"), true, Types.TINYINT),
-		new ColumnType("SMALLINT", Messages.getResourceString("type.integer"), true, Types.SMALLINT),
-		new ColumnType("BIGINT", Messages.getResourceString("type.integer"), true, Types.BIGINT),
 		new ColumnType("REAL", Messages.getResourceString("type.real"), true, Types.REAL),
+		new ColumnType("FLOAT", Messages.getResourceString("type.real"), false, Types.FLOAT),
+		new ColumnType("DOUBLE PRECISION", Messages.getResourceString("type.real"), false, Types.DOUBLE),
+		new ColumnType("CHAR", Messages.getResourceString("type.char"), true, Types.CHAR),
+		new ColumnType("VARCHAR", Messages.getResourceString("type.string"),true, Types.VARCHAR),
+		new ColumnType("LONGVARCHAR", Messages.getResourceString("type.string"),false, Types.VARCHAR),
+		new ColumnType("CLOB", Messages.getResourceString("type.string"), true, Types.CLOB),
 		new ColumnType("BINARY", Messages.getResourceString("type.binary"), true, Types.BINARY),
 		new ColumnType("VARBINARY", Messages.getResourceString("type.binary"), true, Types.BINARY),
-		new ColumnType("LONGVARBINARY", Messages.getResourceString("type.binary"), true, Types.BINARY),
+		new ColumnType("LONGVARBINARY", Messages.getResourceString("type.binary"), false, Types.BINARY),
+		new ColumnType("BIT", Messages.getResourceString("type.bit"), true, Types.BIT),
+		new ColumnType("BIT VARYING", Messages.getResourceString("type.bit"), true, Types.BIT),
 		new ColumnType("DATE", Messages.getResourceString("type.date"), false, Types.DATE),
-		new ColumnType("TIME", Messages.getResourceString("type.time"), false, Types.TIME),
-		new ColumnType("TIMESTAMP", Messages.getResourceString("type.datetime"), false, Types.TIMESTAMP),
-		new ColumnType("DATETIME", Messages.getResourceString("type.datetime"), false, Types.TIMESTAMP),
-		new ColumnType("OTHER", Messages.getResourceString("type.other"), false, Types.OTHER),
-		new ColumnType("OBJECT", Messages.getResourceString("type.object"), false, Types.OTHER)
+		new ColumnType("TIME", Messages.getResourceString("type.time"), true, Types.TIME),
+		new ColumnType("TIMESTAMP", Messages.getResourceString("type.datetime"), true, Types.TIMESTAMP),
+		new ColumnType("INTERVAL", Messages.getResourceString("type.other"), false, Types.OTHER),
+		new ColumnType("BOOLEAN", Messages.getResourceString("type.boolean"), false, Types.BOOLEAN),
+		new ColumnType("OTHER", Messages.getResourceString("type.other"), false, Types.OTHER)
+		// TODO: UUID, ARRAY
 	};
 
 	public HsqldbDialect(){
